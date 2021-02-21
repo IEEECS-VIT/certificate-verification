1d6017bbc04bd7ffd9d463128cc9846cd628a33e
harshkanani014 <harshkanani100@gmail.com>
2021-02-21 23:20:13 +0530

    added qr-code

--- get_data/views.py
+++ get_data/views.py
@@ -5,6 +5,8 @@ import base64
 from io import BytesIO
 from django.http import HttpResponse, FileResponse
 import img2pdf
+import pyqrcode
+#import qrcode
 # Create cyour views here.
 
 def generate_certificate(request, slug):
@@ -21,9 +23,20 @@ def generate_certificate(request, slug):
         text_color = (0, 137, 209)
         font = ImageFont.truetype("arial.ttf", 120)
         d.text(location, user_data.Full_Name, fill=text_color, font=font)
+        url = pyqrcode.QRCode("http://127.0.0.1:8000/get_data/generate_certificate/"+str(slug))
+        url.png('test.png', scale=5)
+        qr = Image.open('test.png')
+        qr = qr.convert("RGBA")
+        im = im.convert("RGBA")
+        box = (0, 0, 265, 265)
+        #qr.crop(box)
+        #region = im
+        print(im)
+        print(qr)
+        im.paste(qr, box)
+        #im.show()
         response = HttpResponse(content_type='image/png')
         im.save(response, "PNG")
-
         return response
 
 def convert_certificate_to_pdf(request, slug):
