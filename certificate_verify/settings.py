de8cdd573adc647f9beb6cf79d10904c078b368f
harshkanani014 <harshkanani100@gmail.com>
2021-03-21 00:12:28 +0530

    third

--- certificate_verify/settings.py
+++ certificate_verify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'u30#gb*l*zu1vp)m@ymi21dh9whs!)acw-3dgm$&t@mm71bm)k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ieee-cs-cert-verify.herokuapp.com"]
 
 
 # Application definition
--- certificate_verify/urls.py
+++ certificate_verify/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('upload_data/', include('upload_data.urls')),
     path('', include('get_data.urls')),
     path('', include('get_board_data.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
