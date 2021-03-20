904c80ac4e0f5afe4e8da123ca5e454217652362
harshkanani014 <harshkanani100@gmail.com>
2021-03-20 15:50:38 +0530

    csv upload fixed

--- upload_data/admin.py
+++ upload_data/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from .models import ParticipantData, Certificate, FontStyle
-from import_export import resources
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
 from import_export.admin import ImportExportActionModelAdmin
 # Register your models here.
 
+
+
 class ParticipantResource(resources.ModelResource):
+    Event_Name = fields.Field(column_name='Event_Name', attribute='Event_Name', widget=ForeignKeyWidget(Certificate, 'Event_Name'))
     class Meta:
         model = ParticipantData
-        fields = ('id', 'Full_Name', 'Event_Name', "Description")
+        import_id_fields = ('Full_Name', 'Event_Name', "Description")
+        fields = ('Full_Name', 'Event_Name', "Description")
+        
 
 class ParticipantDataAdmin(ImportExportActionModelAdmin):
     resource_class = ParticipantResource
-    exclude = ['slug']
+    exclude = ('id', 'slug')
+    
 
 
 
