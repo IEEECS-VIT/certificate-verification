6022b0d3dbff41d570395fad8a4edd544728cb06
harshkanani014 <harshkanani100@gmail.com>
2021-03-28 17:57:27 +0530

    wnfnnfw

--- get_data/views.py
+++ get_data/views.py
@@ -25,7 +25,7 @@ def generate_certificate(request, slug):
         font_style = FontStyle.objects.get(id=certificate_data.font_type_id)
         #print(font_style.font_type)
         
-        font = ImageFont.truetype("/get_data/arial.ttf", certificate_data.font_size) # set font size
+        font = ImageFont.truetype("get_data/arial.ttf", certificate_data.font_size) # set font size
          
         ######## write text i.e full name, event name  into certificate
         d.text(participate_name_location, user_data.Full_Name, fill=text_color, font=font) 
