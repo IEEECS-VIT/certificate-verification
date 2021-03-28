dfa45cafe17ea4351f01761b965fbf95cb36f2a2
harshkanani014 <harshkanani100@gmail.com>
2021-03-28 11:21:40 +0530

    abcd

--- certificate_verify/settings.py
+++ certificate_verify/settings.py
@@ -15,7 +15,6 @@ import os
 import django_heroku
 import dj_database_url
 import environ
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
